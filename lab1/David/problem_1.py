22c30ba7fa0b12e92875c3449df536584bde52f4
David <davidrom@kth.se>
2021-11-23 17:40:46 +0100

    Modified code in maze.py as well as jupyter notebook to solve questions 1c-1e

---
+++ lab1/David/problem_1.py
@@ -0,0 +1,31 @@
+import maze as mz
+import numpy as np
+from IPython import display
+
+# Modified maze from lab0. Now corresponding to maze in lab1
+
+maze = np.array([
+    [0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 1, 0, 0, 1, 1, 1],
+    [0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 1, 1, 1, 1, 1, 0],
+    [0, 0, 0, 0, 1, 2, 0, 0]
+])
+
+env = mz.Maze(maze, stand_still=True, discount=30)
+mz.dynamic_programming(env, 30)
+
+
+# Finite horizon
+horizon = 30
+# Solve the MDP problem with dynamic programming
+V, policy = mz.dynamic_programming(env, horizon)
+
+# Simulate the shortest path starting from position A
+method = 'DynProg'
+start = (0, 0, 6, 5)
+path = env.simulate(start, policy, method)
+
+mz.policy_evaluation(env, policy, horizon)
